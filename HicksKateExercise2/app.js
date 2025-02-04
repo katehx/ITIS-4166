3441f29b13da31b3ffaa4c9264558bce57962caa
Kate Hicks <Katehicks329@gmail.com>
2025-02-03 20:54:52 -0500

    Exercise 2 done

--- HicksKateExercise2/app.js
+++ HicksKateExercise2/app.js
@@ -1,14 +1,27 @@
 const http = require('http');
 const fs = require('fs');
 
-const port = 1234;
+const port = 3000;
 const host = 'localhost';
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./views/index.html', (err, data) => {
+
+    let path = './views/';
+
+    if(req.url === '/') {
+        path = path + 'index.html';
+    } else if(req.url === '/contact') {
+        path = path + 'contact.html';
+    } else {
+        res.statusCode = 404;
+        path = path + '404.html';
+    }
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
+            res.end();
         } else {
             res.write(data);
             res.end();

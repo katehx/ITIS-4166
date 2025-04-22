b6cd6347d44baf22a351b70f861efeeb1a408424
Kate Hicks <Katehicks329@gmail.com>
2025-04-22 18:32:03 -0400

    Exercise 9

---
+++ KateHicksExercise9/routes/storyRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const controller = require('../controllers/storyController');
+const {isLoggedIn, isAuthor} = require('../middlewares/auth');
+const { validateId } = require('../middlewares/validator');
+const { validateStory, validateResult } = require('../middlewares/validator');
+
+
+const router = express.Router();
+
+router.get('/', controller.index);
+router.get('/new', isLoggedIn, controller.new);
+router.post('/', isLoggedIn, validateStory, validateResult, controller.create);
+router.get('/:id', validateId, controller.show);
+router.get('/:id/edit', validateId, isLoggedIn, isAuthor, controller.edit);
+router.put('/:id', validateId, isLoggedIn, isAuthor, validateStory, validateResult, controller.update);
+router.delete('/:id', validateId, isLoggedIn, isAuthor, controller.delete);
+
+module.exports = router;
